ea0230c85bb9e49e08d32e06ce68874fcb980d75
Dave Page <dpage@pgadmin.org>
2026-03-11 15:33:26 +0000

    More search result layout improvements.

--- docs/javascripts/pagefind-search.js
+++ docs/javascripts/pagefind-search.js
@@ -17,11 +17,13 @@
         modal.setAttribute('aria-label', 'Search');
         modal.innerHTML =
             '<div class="pagefind-modal-container">' +
-                '<button class="pagefind-modal-close" aria-label="Close search">' +
-                    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">' +
-                        '<path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" fill="currentColor"/>' +
-                    '</svg>' +
-                '</button>' +
+                '<div class="pagefind-modal-header">' +
+                    '<button class="pagefind-modal-close" aria-label="Close search">' +
+                        '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20">' +
+                            '<path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" fill="currentColor"/>' +
+                        '</svg>' +
+                    '</button>' +
+                '</div>' +
                 '<div id="pagefind-container"></div>' +
             '</div>';
 
